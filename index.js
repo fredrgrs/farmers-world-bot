86959be010362137d84efc4c730b7125442fd2d1
Fred <fredrgrs42@gmail.com>
2022-03-05 12:04:03 -0500

    Change some delays; use mapId not availableMaps

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 (async () => {
-  // Maps on which you can collect resources
-  // 1 — Mining
-  // 2 — Chiken
-  // 3 — Plant
-  // 4 — Cow
-  const availableMaps = [1, 2, 3, 4];
-  // Delay between moving to the next map [min, max]
-  const delayNextMap = [10 * 1000, 15 * 1000];
+  // mapIds
+  // 0 — Mining
+  // 1 — Chicken
+  // 2 — Plant
+  // 3 — Cow
+  const availableMaps = [0, 1, 2, 3];
+  // Delay before moving to the next map [min, max]
+  const delayNextMap = [2 * 1000, 5 * 1000];
   // Delay after map selection [min, max]
-  const delayAfterMapSelect = [5 * 1000, 10 * 1000];
+  const delayAfterMapSelect = [2 * 1000, 4 * 1000];
   // Delay after mine [min, max]
-  const delayAfterMine = [15 * 1000, 20 * 1000];
+  const delayAfterMine = [20 * 1000, 30 * 1000];
   // Delay after repair [min, max]
-  const delayAfterRepair = [10 * 1000, 15 * 1000];
+  const delayAfterRepair = [20 * 1000, 30 * 1000];
 
   const random = (min, max) =>
     Math.floor(Math.random() * (max - min + 1) + min);
@@ -23,7 +23,7 @@
     );
 
     if (buttonClosePopup) {
-      console.log("Closing popup, modal .plain-button.short");
+      //console.log("Closing popup, modal .plain-button.short");
       buttonClosePopup.click();
     }
   }, random(1, 2) * 1000);
@@ -43,16 +43,12 @@
     try {
       // For each map
       for (let mapId = 0; mapId < 4; ++mapId) {
-        if (!availableMaps.includes(mapId + 1)) continue;
-
+        if (!availableMaps.includes(mapId)) continue;
         await new Promise((res) => setTimeout(res, random(...delayNextMap)));
-
         const map = document.querySelectorAll(".map-container-bg")[mapId];
-
         if (map.style.filter === "grayscale(1)") continue;
-
         map.click();
-        console.log("I Switched to map: " + mapId);
+        //console.log("I Switched to map: " + mapId);
 
         await new Promise((res) =>
           setTimeout(res, random(...delayAfterMapSelect))
@@ -93,7 +89,7 @@
               console.log("*** Restoring energy from " + currentEnergy);
               document.querySelector(".modal-wrapper .plain-button").click();
               await new Promise((res) =>
-                setTimeout(res, random(15, 15) * 1000)
+                setTimeout(res, random(15, 20) * 1000)
               );
             }
           }
@@ -112,7 +108,7 @@
             ".info-section .info-time"
           ).innerText;
 
-          console.log("Looking at card: " + cardName);
+          //console.log("Looking at card: " + cardName);
 
           // Repair tool if needed
           if (mapId === 0) {
@@ -137,7 +133,7 @@
 
           // Skip if disabled
           if ([...buttonMine.classList].includes("disabled")) {
-            console.log("Mine button disabled, skipping");
+            //console.log("Mine button disabled, skipping");
             continue;
           }
 
@@ -147,25 +143,26 @@
               ".info-section .info-title-level"
             );
             if (!mineLevelDiv) {
-              console.log("Member NFT click: " + document.querySelector(
+              console.log("Member click: " + document.querySelector(
               ".info-section .info-title-name").innerText);
               buttonMine.click();
             } else {
               const mineLevels = mineLevelDiv.innerText.split("/");
               if (mineLevels[0] === mineLevels[1]) {
-                console.log("CLICK Tool NFT: " + document.querySelector(
+                console.log("Tool click: " + document.querySelector(
                             ".info-section .info-title-name").innerText
                             + ", mineLevels: " + mineLevels);
                 buttonMine.click();
               } else {
-                console.log("Skipping Tool: " + document.querySelector(
-                            ".info-section .info-title-name").innerText
-                            + ", mineLevels: " + mineLevels);
+                //console.log("Skipping Tool: " + document.querySelector(
+                            //".info-section .info-title-name").innerText
+                            //+ ", mineLevels: " + mineLevels);
                 continue;
               }
             }
           } else if (timeToEnd === "00:00:00") {
-            console.log("00:00:00 time left, time to click");
+            console.log("Crop/Animal click: " + document.querySelector(
+                            ".info-section .info-title-name").innerText);
             buttonMine.click();
           } else {
             console.log("Not ready to click, skipping"); // TODO: probably can't get here
